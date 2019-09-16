9d6851834bad5311732b83f2295cf53e8e4d5a97
vaibhavukarande <vaibhavukarande@users.noreply.github.com>
2019-09-16 10:30:41 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,32 @@
+# --------------
+import yaml
+
+# Read the data of the format .yaml type
+f=open(path,'r')
+print(f.read())
+
+# Find data type of the file
+
+
+# In which city, and at which venue the match was played and where was it played ?
+
+
+# Which are all the teams that played in the tournament ? How many teams participated in total?
+
+
+# Which team won the toss and what was the decision of toss winner ?
+
+
+# Find the first bowler and first batsman who played the first ball of the first inning
+
+
+# How many deliveries were delivered in first inning ?
+
+
+# How many deliveries were delivered in second inning ?
+
+
+# Which team won and how ?
+
+
+
